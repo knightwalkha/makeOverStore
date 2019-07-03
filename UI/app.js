10f5030384651db04f1397af882856c69dab5b12
knightwalkha <ohiosumuankehinde@gmail.com>
2019-07-03 15:16:53 +0100

    app.js added

--- UI/app.js
+++ UI/app.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
-    host:localhost,
-    user:root,
-    password:khennikay101,
-    database:login_signupdb
+    host:'localhost',
+    user:'root',
+    password:'khennikay101',
+    database:'login_signupdb'
 });
 
 mysqlConnection.connect((err) => {
     if(!err)
         console.log('DB connection successful.');
     else
-        console.log('DB connection failed \n Error: ' = JSON.stringify(err, undefined, 2));
+        console.log('DB connection failed \n Error: ' + JSON.stringify(err, undefined, 2));
 });
